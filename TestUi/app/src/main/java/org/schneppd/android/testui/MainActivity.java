2a49432e863e19ab906bb14d11bf45ca1b54280e
schneppd <david.schnepp@schneppd.com>
2017-05-16 16:47:33 +0200

    Added player index activity ui

--- TestUi/app/src/main/java/org/schneppd/android/testui/MainActivity.java
+++ TestUi/app/src/main/java/org/schneppd/android/testui/MainActivity.java
@@ -1,5 +1,6 @@
 package org.schneppd.android.testui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,12 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.ToolbarActivityTitle);
-        setSupportActionBar(toolbar);
-        // remove default behaviours
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setDisplayShowHomeEnabled(false);
 
+    }
+
+    public void ExecuteStartGame(View v){
+        Intent i = new Intent(getBaseContext(), PlayerIndexActivity.class);
+        startActivity(i);
     }
 
 }
---
+++ TestUi/app/src/main/java/org/schneppd/android/testui/PlayerIndexActivity.java
@@ -0,0 +1,26 @@
+package org.schneppd.android.testui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PlayerIndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_index);
+        // set ToolbarActivityTitle as activity's actionbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.ToolbarActivityTitle);
+        setSupportActionBar(toolbar);
+        // remove default behaviours
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+
+
+    }
+
+}
